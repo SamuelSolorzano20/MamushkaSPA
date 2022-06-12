adb8e5e688013381a45b3d377f1efc647c892f53
emiranda <ernestolebni123@gmail.com>
2022-06-12 09:02:50 -0600

    Contact form is Functional

--- src/app/components/home/contact/contact.component.ts
+++ src/app/components/home/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,10 +9,26 @@ import { FormGroup } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
-  form:FormGroup = new FormGroup({});
-  constructor() { }
+  form:FormGroup = new FormGroup({
+    Name: new FormControl(''),
+    Email:new FormControl(''),
+    PhoneNumber:new FormControl(''),
+    Comments:new FormControl(''),
+  });
+  constructor(private readonly db:AngularFirestore) { }
 
   ngOnInit(): void {
   }
-
+  async Submit()
+  {
+    if (this.form.value){
+        const inserted = await this.db.collection('Contact').add(this.form.value);
+        if(inserted){
+          this.form.reset();
+        }
+        else{
+          return;
+        }
+    }
+  }
 }
---
+++ src/app/components/models/contacts.ts
@@ -0,0 +1,7 @@
+export class Contact
+{
+    constructor(public Name:string,
+                public Email:string,
+                public PhoneNumber:string,
+                public Comments:string) {}
+}
